5891063ad91b5521effc0bef3284ff5d209e5cea
Jomon Johnson <jomon.entero@gmail.com>
2019-02-14 21:58:56 +0530

    init

---
+++ src/config/cron.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 14/2/19
+ * Time: 9:45 PM
+ */
+
+return [
+    'modules' => [
+        'cron' => [
+            'class' => \vm\cron\CronModule::class,
+        ],
+    ],
+];
